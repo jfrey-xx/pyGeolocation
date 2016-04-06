e00c36cb4c447e159d90855f377660fbeabb0995
moneeb777 <moneeb777@hotmail.com>
2016-04-06 11:46:26 +0500

    Better error handling

--- geolocate.py
+++ geolocate.py
@@ -61,21 +61,20 @@ class geolocate:
         self.url = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + self.key
         self.gmaps = googlemaps.Client(key=self.key)  # For reverse geocoding google geolocation response
 
-
         self.response = []
         self.payload = {}
 
         self.WifiObject = WifiGrab()  # to grab BSSIDs
         self.ssids, self.bssids, self.rssi, self.channel = self.WifiObject.all()
 
-    def buildJSON(self):
+    def buildJSON(self, considerIP='true'):
         if len(self.bssids) < 3:
             print 'Atleast two BSSIDs are required.'
             print 'Current number of bssids: %i' % len(self.bssids)
             return
 
         # Building payload as per google-guidelines
-        self.payload['considerIp'] = 'true'
+        self.payload['considerIp'] = considerIP
         self.payload['wifiAccessPoints'] = []
         for i in range(0,len(self.bssids)):
             self.payload["wifiAccessPoints"].append({})
@@ -97,6 +96,21 @@ class geolocate:
     def request(self):  # Send request to geolocate and return the response
         try:
             self.response = requests.post(self.url, data=json.dumps(self.payload), headers=self.headers)
+            text = json.loads(self.response.text)
+
+            if self.response.ok == False:  # Check if the response was ok
+                if text['error']['errors'][0]['reason'] == 'dailyLimitExceeded':
+                    print 'You have exceeded you daily limit'
+                elif text['error']['errors'][0]['reason'] == 'keyInvalid':
+                    print 'Your API key is not valid for the Google Maps Geolocation API'
+                elif text['error']['errors'][0]['reason'] == 'userRateLimitExceeded':
+                    print 'You\'ve exceeded the requests per second per user limit that you configured in the Google Developers Console'
+                elif text['error']['errors'][0]['reason'] == 'notFound':
+                    print 'The request was valid, but no results were returned'
+                elif text['error']['errors'][0]['reason'] == 'parseError':
+                    print 'The request body is not valid JSON'
+                else:
+                    print 'Unknown error in the geolocation response. Might be caught in an exception.'
         except Exception, e:
             print str(e)
 
@@ -124,9 +138,9 @@ class geolocate:
         return lat, lng, accuracy
 
 geolocateMe = geolocate(geolocateApiKey)    # replace geolocateApiKey variable at the top with your API key
-geolocateMe.buildJSON()                     # build JSON request object
+geolocateMe.buildJSON(condiderIP='true')    # build JSON request object
 geolocateMe.request()                       # Send request to google
-# geolocateMe.printResponse()                 # Print response from google
+# geolocateMe.printResponse()               # Print response from google
 
 # Printing
 lat, lng, accuracy = geolocateMe.getLongLat()
